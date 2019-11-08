7bded6dcdeac83251719e85e849a06edf83da378
kh45 <khaledhassan45@gmail.com>
2019-11-08 16:39:47 -0500

    deck and user models

--- app/controllers/decks_controller.rb
+++ app/controllers/decks_controller.rb
@@ -1,2 +1,20 @@
 class DecksController < ApplicationController
+
+    def show
+        deck = Deck.find(params[:id])
+        
+        render json: deck.to_json(to_serialized_json)
+    end
+
+    private
+
+    def to_serialized_json
+        {
+    :include => {:cards => {
+            :except => [:created_at, :updated_at]
+        }},
+        :except => [:created_at, :updated_at]
+    }
+    end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,22 @@
 class UsersController < ApplicationController
 
-    def index
-        cards = Card.all
-        render json: cards
+    def show
+        user = User.find(params[:id])
+        render json: user.to_json(to_serialized_json)
+    end
+
+
+    private
+
+    def to_serialized_json
+        {
+    :include => {:decks => {
+        :include => {:cards => {
+            :except => [:created_at, :updated_at]
+        }},
+        :except => [:created_at, :updated_at]
+    }
+}, :except => [:created_at, :updated_at]
+}
     end
 end
