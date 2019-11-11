97beed9b50324bf6b74824b1dc0eda4055208fee
Nick <nboober@terpmail.umd.edu>
2019-11-11 10:25:51 -0500

    updated migration data

--- db/seeds.rb
+++ db/seeds.rb
@@ -29,12 +29,14 @@ deck2_cards = JSON.parse(raw_cards_deck2)
 
 60.times do
     random_card = deck1_cards["cards"].sample
-    Card.create!(name: random_card["name"], imageUrl: random_card["imageUrlHiRes"], hp: random_card["hp"].to_i, attack_name: random_card["attacks"][0]["name"], attack_damage: random_card["attacks"][0]["damage"].to_i, deck: deck1)
+    Card.create!(name: random_card["name"], imageUrl: random_card["imageUrlHiRes"], hp: random_card["hp"].to_i, attack_name: random_card["attacks"][0]["name"], attack_damage: random_card["attacks"][0]["damage"].to_i,
+    attack_name_2: random_card["attacks"][1]["name"], attack_damage_2: random_card["attacks"][1]["damage"].to_i, deck: deck1)
 end
 
 60.times do
     random_card = deck2_cards["cards"].sample
-    Card.create!(name: random_card["name"], imageUrl: random_card["imageUrlHiRes"], hp: random_card["hp"].to_i, attack_name: random_card["attacks"][0]["name"], attack_damage: random_card["attacks"][0]["damage"].to_i, deck: deck2)
+    Card.create!(name: random_card["name"], imageUrl: random_card["imageUrlHiRes"], hp: random_card["hp"].to_i, attack_name: random_card["attacks"][0]["name"], attack_damage: random_card["attacks"][0]["damage"].to_i,
+    attack_name_2: random_card["attacks"][1]["name"], attack_damage_2: random_card["attacks"][1]["damage"].to_i, deck: deck2)
 end
 # byebug
 
