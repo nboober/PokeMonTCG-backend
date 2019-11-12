2b72dbbf4e07f261cd88ca2a0362735dabf89d8a
kh45 <khaledhassan45@gmail.com>
2019-11-12 09:15:44 -0500

    made login page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_08_180451) do
+ActiveRecord::Schema.define(version: 2019_11_12_022404) do
 
   create_table "cards", force: :cascade do |t|
     t.string "name"
@@ -36,6 +36,8 @@ ActiveRecord::Schema.define(version: 2019_11_08_180451) do
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "username"
+    t.string "password"
   end
 
 end
