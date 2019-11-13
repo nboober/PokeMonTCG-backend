b621359b45b17d77a2f3268191f6154ad66c69e1
kh45 <khaledhassan45@gmail.com>
2019-11-13 16:46:18 -0500

    working create new deck

--- app/controllers/decks_controller.rb
+++ app/controllers/decks_controller.rb
@@ -7,14 +7,13 @@ class DecksController < ApplicationController
         cards_hash = JSON.parse(raw_cards)
         cards = cards_hash["cards"]
 
-
         face_down = cards.select{|card| card["name"] == "Ho-Oh BREAK"} #removing face down card
         cards.delete(face_down[0]) #removing face down card
         
-        60.times do
+        while deck.cards.count < 60 do
             random_card = cards.sample
             
-            no_attack = {name: "No Attack", damage: 0}
+            no_attack = {"name" => "No Attack", "damage" => 0}
         
             if random_card["attacks"][1] == nil
                 random_card["attacks"].push(no_attack)
@@ -23,6 +22,7 @@ class DecksController < ApplicationController
             Card.create!(name: random_card["name"], imageUrl: random_card["imageUrlHiRes"], hp: random_card["hp"].to_i, attack_name: random_card["attacks"][0]["name"], attack_damage: random_card["attacks"][0]["damage"].to_i,
             attack_name_2: random_card["attacks"][1]["name"], attack_damage_2: random_card["attacks"][1]["damage"].to_i, deck: deck)
         end
+        render json: deck.to_json(to_serialized_json)
     end
     
     
